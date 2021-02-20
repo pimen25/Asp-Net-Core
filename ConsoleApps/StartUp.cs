3863f287051e477f1091dccc53227dfd1c1df3a9
Nikita Nikita <zorro.pimenov@gmail.com>
2021-02-21 01:19:24 +0300

    d/z

---
+++ ConsoleApps/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApps
+{
+    public class StartUp
+    {
+        public void ConfigurationServices(IServiceCollection servise)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app,IWebHostEnvironment env )
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello");
+                });
+            });
+        }
+    }
+}
